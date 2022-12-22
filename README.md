775164d0e0a9217ebb665fdaf8e5ae0f1853782e
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-12-22 11:49:14 +0100

    added routes and tests

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ```bash
 uvicorn app.main:app --reload
 ```
+
+Apply migrations
+
+```bash
+docker-compose exec web aerich upgrade
+```
+
+Run Tests
+
+```bash
+docker-compose exec web python -m pytest -p no:warnings
+```
--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,8 @@
 import logging
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 
@@ -13,6 +12,7 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
