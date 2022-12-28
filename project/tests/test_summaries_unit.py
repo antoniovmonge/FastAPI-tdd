8a62e724e1cb60be2bc9c3ab40005cf2a6485d30
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-12-28 19:16:46 +0100

    added summarizer functionality

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -15,6 +15,11 @@ def test_create_summary(test_app, monkeypatch):
 
     monkeypatch.setattr(crud, "post", mock_post)
 
+    def mock_generate_summary(summary_id, url):
+        return None
+
+    monkeypatch.setattr(summaries, "generate_summary", mock_generate_summary)
+
     response = test_app.post(
         "/summaries/",
         data=json.dumps(test_request_payload),
@@ -219,7 +224,7 @@ def test_update_summary_invalid(
 
 def test_update_summary_invalid_url(test_app):
     response = test_app.put(
-        f"/summaries/1/",
+        "/summaries/1/",
         data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
